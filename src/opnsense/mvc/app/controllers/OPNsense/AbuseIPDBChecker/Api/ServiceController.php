7efc00fef8080c784d8f151c4304311fb9c1d0ac
JohnTheWizard <j.sulwessak@gmail.com>
2025-03-22 00:03:15 +0100

    v0.3

--- src/opnsense/mvc/app/controllers/OPNsense/AbuseIPDBChecker/Api/ServiceController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/AbuseIPDBChecker/Api/ServiceController.php
@@ -1,59 +1,26 @@
 <?php
 namespace OPNsense\AbuseIPDBChecker\Api;
 
-use OPNsense\Base\ApiControllerBase;
+use OPNsense\Base\ApiMutableServiceControllerBase;
 use OPNsense\Core\Backend;
 use OPNsense\Core\Config;
 use OPNsense\AbuseIPDBChecker\AbuseIPDBChecker;
 
-class ServiceController extends ApiControllerBase
+class ServiceController extends ApiMutableServiceControllerBase
 {
-    /**
-     * Reconfigure AbuseIPDB Checker
-     * @return array
-     */
-    public function reconfigureAction()
-    {
-        if ($this->request->isPost()) {
-            // Generate config file using the template system
-            $backend = new Backend();
-            $response = $backend->configdRun('template reload OPNsense/AbuseIPDBChecker');
-            
-            // Restart service if model is enabled
-            $model = new AbuseIPDBChecker();
-            if ((string)$model->general->enabled == '1') {
-                $response = $backend->configdRun('service restart abuseipdbchecker');
-            } else {
-                $response = $backend->configdRun('service stop abuseipdbchecker');
-            }
-            
-            return ['status' => 'ok', 'message' => $response];
-        }
-        
-        return ['status' => 'failed', 'message' => 'Only POST requests allowed'];
-    }
+    protected static $internalServiceClass = '\OPNsense\AbuseIPDBChecker\AbuseIPDBChecker';
+    protected static $internalServiceTemplate = 'OPNsense/AbuseIPDBChecker';
+    protected static $internalServiceEnabled = 'general.enabled';
+    protected static $internalServiceName = 'abuseipdbchecker';
     
     /**
-     * Get service status
-     * @return array
+     * Determine if we need to force restart on reconfigure
      */
-    public function statusAction()
+    protected function reconfigureForceRestart()
     {
-        $backend = new Backend();
-        $model = new AbuseIPDBChecker();
-        $response = [];
-        
-        if ((string)$model->general->enabled == '1') {
-            $response['status'] = trim($backend->configdRun('service status abuseipdbchecker'));
-            $response['enabled'] = true;
-        } else {
-            $response['status'] = 'disabled';
-            $response['enabled'] = false;
-        }
-        
-        return $response;
+        return 0;
     }
-
+    
     /**
      * Run AbuseIPDB Checker manually
      * @return array
--- src/opnsense/mvc/app/controllers/OPNsense/AbuseIPDBChecker/GeneralController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/AbuseIPDBChecker/GeneralController.php
@@ -6,11 +6,11 @@
 
 namespace OPNsense\AbuseIPDBChecker;
 
-use OPNsense\Base\ControllerBase;
+use OPNsense\Base\IndexController;
 use OPNsense\Core\Config;
 use OPNsense\AbuseIPDBChecker\AbuseIPDBChecker;
 
-class GeneralController extends ControllerBase
+class GeneralController extends IndexController
 {
     /**
      * Main index page
@@ -20,5 +20,8 @@ class GeneralController extends ControllerBase
     {
         // Render the main index view
         $this->view->pick('OPNsense/AbuseIPDBChecker/index');
+        
+        // Load form data - this is key for populating the form
+        $this->view->generalForm = $this->getForm("general");
     }
 }
